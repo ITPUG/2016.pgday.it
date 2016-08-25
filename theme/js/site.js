b0d9540bd14c317d9690b154506dd2c68a48e56f
Gianluca Riccardi <gianluca@mwd2>
2016-08-25 12:48:27 +0200

    Initial commit og 2016.PGDay.IT website

---
+++ theme/js/site.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var map_container = document.getElementById('js-site-event-map');
+    if (map_container)
+        init_map(map_container);
+});
